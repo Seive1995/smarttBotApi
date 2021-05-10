3c3a52afe8665ad6bfde60b7733876760b7d5b88
Seive1995 <romaricpecg2015@gmail.com>
2021-05-10 19:33:02 -0300

    Corrigindo a atualizacao dos dados dos candles

--- app/app.py
+++ app/app.py
@@ -20,8 +20,17 @@ db = create_engine(db_string)
 
 dadosAtualizadoPorSegundo = {}
 dadosAtualizadoAberturaCadaMinuto = {}
+dadosAtualizadoMinimoCadaMinuto = {}
+dadosAtualizadoMaximoCadaMinuto = {}
+dadosAtualizadoFechaduraCadaMinuto = {}
 dadosAtualizadoAberturaCadaCincoMinutos = {}
+dadosAtualizadoMinimoCadaCincoMinutos = {}
+dadosAtualizadoMaximoCadaCincoMinutos = {}
+dadosAtualizadoFechaduraCadaCincoMinutos = {}
 dadosAtualizadoAberturaCadaDezMinutos = {}
+dadosAtualizadoMinimoCadaDezMinutos = {}
+dadosAtualizadoMaximoCadaDezMinutos = {}
+dadosAtualizadoFechaduraCadaDezMinutos = {}
 minutoAnterior = -1
 
 
@@ -29,32 +38,123 @@ def obterPolinexApiDados():
     response = requests.get("https://poloniex.com/public?command=returnTicker")
     return response
 
-def inicializarfechadurasCandles(key, value):
+def inicializarDados(key, value):
     global dadosAtualizadoAberturaCadaMinuto
+    global dadosAtualizadoMinimoCadaMinuto
+    global dadosAtualizadoMaximoCadaMinuto
+    global dadosAtualizadoFechaduraCadaMinuto
     global dadosAtualizadoAberturaCadaCincoMinutos
-    global dadosAtualizadoAberturaCadaDezMinutoe
+    global dadosAtualizadoMinimoCadaCincoMinutos
+    global dadosAtualizadoMaximoCadaCincoMinutos
+    global dadosAtualizadoFechaduraCadaCincoMinutos
+    global dadosAtualizadoAberturaCadaDezMinutos
+    global dadosAtualizadoMinimoCadaDezMinutos
+    global dadosAtualizadoMaximoCadaDezMinutos
+    global dadosAtualizadoFechaduraCadaDezMinutos
+
    
-    dadosAtualizadoAberturaCadaMinuto[key] = value['last']
+    dadosAtualizadoAberturaCadaMinuto[key] = value['last'] 
+    dadosAtualizadoMinimoCadaMinuto[key] = value['last']
+    dadosAtualizadoMaximoCadaMinuto[key] = value['last']
+    dadosAtualizadoFechaduraCadaMinuto[key] = value['last']
     dadosAtualizadoAberturaCadaCincoMinutos[key] = value['last']
+    dadosAtualizadoMinimoCadaCincoMinutos[key] = value['last']
+    dadosAtualizadoMaximoCadaCincoMinutos[key] = value['last']
+    dadosAtualizadoFechaduraCadaCincoMinutos[key] = value['last']
     dadosAtualizadoAberturaCadaDezMinutos[key] = value['last']
+    dadosAtualizadoMinimoCadaDezMinutos[key] = value['last']
+    dadosAtualizadoMaximoCadaDezMinutos[key] = value['last']
+    dadosAtualizadoFechaduraCadaDezMinutos[key] = value['last']
+
+def resertarCandlesPorPeriodicidade(periodicidade,key, value):
+    global dadosAtualizadoAberturaCadaMinuto
+    global dadosAtualizadoMinimoCadaMinuto
+    global dadosAtualizadoMaximoCadaMinuto
+    global dadosAtualizadoFechaduraCadaMinuto
+    global dadosAtualizadoAberturaCadaCincoMinutos
+    global dadosAtualizadoMinimoCadaCincoMinutos
+    global dadosAtualizadoMaximoCadaCincoMinutos
+    global dadosAtualizadoFechaduraCadaCincoMinutos
+    global dadosAtualizadoAberturaCadaDezMinutos
+    global dadosAtualizadoMinimoCadaDezMinutos
+    global dadosAtualizadoMaximoCadaDezMinutos
+    global dadosAtualizadoFechaduraCadaDezMinutos
+
+    if periodicidade == 1: 
+        dadosAtualizadoAberturaCadaMinuto[key] = value['last']
+        dadosAtualizadoMinimoCadaMinuto[key] = value['last']
+        dadosAtualizadoMaximoCadaMinuto[key] = value['last']
+        dadosAtualizadoFechaduraCadaMinuto[key] = value['last']
     
+    if periodicidade == 5:
+        dadosAtualizadoAberturaCadaCincoMinutos[key] = value['last']
+        dadosAtualizadoMinimoCadaCincoMinutos[key] = value['last']
+        dadosAtualizadoMaximoCadaCincoMinutos[key] = value['last']
+        dadosAtualizadoFechaduraCadaCincoMinutos[key] = value['last']
+    
+    if periodicidade == 10:
+        dadosAtualizadoAberturaCadaDezMinutos[key] = value['last']
+        dadosAtualizadoMinimoCadaDezMinutos[key] = value['last']
+        dadosAtualizadoMaximoCadaDezMinutos[key] = value['last']
+        dadosAtualizadoFechaduraCadaDezMinutos[key] = value['last']
+
+
+def atualizarDadosMinimo(key, value):
+    global dadosAtualizadoMinimoCadaMinuto
+    global dadosAtualizadoMinimoCadaCincoMinutos
+    global dadosAtualizadoMinimoCadaDezMinutos
+
+    if value['last'] < dadosAtualizadoMinimoCadaMinuto[key]:
+        dadosAtualizadoMinimoCadaMinuto[key] = value['last']
+    
+    if value['last'] < dadosAtualizadoMinimoCadaCincoMinutos[key]:
+        dadosAtualizadoMinimoCadaCincoMinutos[key] = value['last']
+
+    if value['last'] < dadosAtualizadoMinimoCadaDezMinutos[key]:
+        dadosAtualizadoMinimoCadaDezMinutos[key] = value['last']
+
+def atualizarDadosMaximo(key, value):
+    global dadosAtualizadoMaximoCadaMinuto
+    global dadosAtualizadoMaximoCadaCincoMinutos
+    global dadosAtualizadoMaximoCadaDezMinutos
+
+    if value['last'] > dadosAtualizadoMaximoCadaMinuto[key]:
+        dadosAtualizadoMaximoCadaMinuto[key] = value['last']
+
+    if value['last'] > dadosAtualizadoMaximoCadaCincoMinutos[key]:
+        dadosAtualizadoMaximoCadaCincoMinutos[key] = value['last']
+
+    if value['last'] > dadosAtualizadoMaximoCadaDezMinutos[key]:
+        dadosAtualizadoMaximoCadaDezMinutos[key] = value['last']
+ 
+def atualizarDadosClose(key, value):
+    global dadosAtualizadoFechaduraCadaMinuto
+    global dadosAtualizadoFechaduraCadaCincoMinutos
+    global dadosAtualizadoFechaduraCadaDezMinutos
+
+    dadosAtualizadoFechaduraCadaMinuto[key] = value['last']
+    dadosAtualizadoFechaduraCadaCincoMinutos[key] = value['last']
+    dadosAtualizadoFechaduraCadaDezMinutos[key] = value['last']
+
+def atualizarDados(key, value):
+    atualizarDadosMinimo(key, value)
+    atualizarDadosMaximo(key, value)
+    atualizarDadosClose(key, value)
+   
 def atualizarDadosCadaSegundo():
     global dadosAtualizadoPorSegundo
     global minutoAnterior
     response = obterPolinexApiDados()
     if dadosAtualizadoPorSegundo == {}:
+        dadosAtualizadoPorSegundo = json.loads(response.text)
         for key, value in json.loads(response.text).items():
-            dadosAtualizadoPorSegundo[key] = {'Open' : value['last'], 'Low': value['last'], 'High': value['last'], 'Close' : value['last']}
-            inicializarfechadurasCandles(key, value)
+            inicializarDados(key, value)
     else:
         #Atualizando o maximo, o minimo e a fechadura a cada segundo para 
         #cada ativo
+        dadosAtualizadoPorSegundo = json.loads(response.text)
         for key, value in json.loads(response.text).items():
-            if float(value['last']) > float(dadosAtualizadoPorSegundo[key]['High']):
-                dadosAtualizadoPorSegundo[key]['High'] = value['last']		
-            if float(value['last']) < float(dadosAtualizadoPorSegundo[key]['Low']):     dadosAtualizadoPorSegundo[key]['Low'] = value['last']	
-
-            dadosAtualizadoPorSegundo[key]['Close'] = value['last']
+            atualizarDados(key, value)
                 
         now = datetime.now()
         #print('datetime :', str(datetime.now()))
@@ -65,46 +165,59 @@ def atualizarDadosCadaSegundo():
 def salvarCandlesCadaMinuto(now):
     global dadosAtualizadoPorSegundo
     global dadosAtualizadoAberturaCadaMinuto
+    global dadosAtualizadoMinimoCadaMinuto
+    global dadosAtualizadoMaximoCadaMinuto
+    global dadosAtualizadoFechaduraCadaMinuto
     print("DateTime: ", str(now))
-    print("1 Minuto :","Open: " +dadosAtualizadoAberturaCadaMinuto['BTC_BTS'] + " Low: " + dadosAtualizadoPorSegundo['BTC_BTS']['Low'] + " High: " + dadosAtualizadoPorSegundo['BTC_BTS']['High'] + " Close: "+ dadosAtualizadoPorSegundo['BTC_BTS']['Close'])
-    #salvarCandlesNoBanco(1, dadosAtualizadoPorSegundo, dadosAtualizadoAberturaCadaMinuto, now)
+    print("1 Minuto :","Open: " + dadosAtualizadoAberturaCadaMinuto['BTC_BTS'] + " Low: " + dadosAtualizadoMinimoCadaMinuto['BTC_BTS'] + " High: " + dadosAtualizadoMaximoCadaMinuto['BTC_BTS'] + " Close: "+ dadosAtualizadoFechaduraCadaMinuto['BTC_BTS'])
+    #salvarCandlesNoBanco(1, dadosAtualizadoAberturaCadaMinuto, dadosAtualizadoMinimoCadaMinuto, dadosAtualizadoMaximoCadaMinuto, dadosAtualizadoFechaduraCadaMinuto, now)
     #Atualizando os dados de abertura a cada minuto para cada ativo
-    for key, value in dadosAtualizadoAberturaCadaMinuto.items():
-        value = dadosAtualizadoPorSegundo[key]['Close']
+    for key, value in dadosAtualizadoPorSegundo.items():
+        resertarCandlesPorPeriodicidade(1, key, value)    
     if now.minute % 5 == 0:
         salvarCandlesCadaCincoMinutos(now)
     
 def salvarCandlesCadaCincoMinutos(now):
     global dadosAtualizadoPorSegundo
     global dadosAtualizadoAberturaCadaCincoMinutos
-    print("5 Minutos :","Open: " +dadosAtualizadoAberturaCadaCincoMinutos['BTC_BTS'] + " Low: " + dadosAtualizadoPorSegundo['BTC_BTS']['Low'] + " High: " + dadosAtualizadoPorSegundo['BTC_BTS']['High'] + " Close: "+ dadosAtualizadoPorSegundo['BTC_BTS']['Close'])
-    #salvarCandlesNoBanco(5, dadosAtualizadoPorSegundo, dadosAtualizadoAberturaCadaCincoMinutos, now)
+    global dadosAtualizadoMinimoCadaCincoMinutos
+    global dadosAtualizadoMaximoCadaCincoMinutos
+    global dadosAtualizadoFechaduraCadaCincoMinutos
+
+    print("5 Minuto :","Open: " + dadosAtualizadoAberturaCadaCincoMinutos['BTC_BTS'] + " Low: " + dadosAtualizadoMinimoCadaCincoMinutos['BTC_BTS'] + " High: " + dadosAtualizadoMaximoCadaCincoMinutos['BTC_BTS'] + " Close: "+ dadosAtualizadoFechaduraCadaCincoMinutos['BTC_BTS'])
+    #salvarCandlesNoBanco(5, dadosAtualizadoAberturaCadaCincoMinutos, dadosAtualizadoMinimoCadaCincoMinutos, dadosAtualizadoMaximoCadaCincoMinutos, dadosAtualizadoFechaduraCadaCincoMinutos, now)
     #Atualizando os dados de abertura a cinco minutos para cada ativo
-    for key, value in dadosAtualizadoAberturaCadaCincoMinutos.items():
-        value = dadosAtualizadoPorSegundo[key]['Close']
+    for key, value in dadosAtualizadoPorSegundo.items():
+        resertarCandlesPorPeriodicidade(5, key, value)            
     if now.minute % 10 == 0:
         salvarCandlesCadaDezMinutos(now)
 
 def salvarCandlesCadaDezMinutos(now):
     global dadosAtualizadoPorSegundo
     global dadosAtualizadoAberturaCadaDezMinutos
-    print("10 Minutos :","Open: " +dadosAtualizadoAberturaCadaDezMinutos['BTC_BTS'] + " Low: " + dadosAtualizadoPorSegundo['BTC_BTS']['Low'] + " High: " + dadosAtualizadoPorSegundo['BTC_BTS']['High'] + " Close: "+ dadosAtualizadoPorSegundo['BTC_BTS']['Close'])
-    #salvarCandlesNoBanco(10, dadosAtualizadoPorSegundo, dadosAtualizadoAberturaCadaDezMinutos, now)
-    #Atualizando os dados de abertura a Dez minutos para cada ativo
-    for key, value in dadosAtualizadoAberturaCadaDezMinutos.items():
-        value = dadosAtualizadoPorSegundo[key]['Close']
+    global dadosAtualizadoMinimoCadaDezMinutos
+    global dadosAtualizadoMaximoCadaDezMinutos
+    global dadosAtualizadoFechaduraCadaDezMinutos
 
-def salvarCandlesNoBanco(periodicidade, dadosAtualizados, dadosAbertura, now):
-    for key, value in dadosAtualizados.items():
+    print("10 Minuto :","Open: " + dadosAtualizadoAberturaCadaDezMinutos['BTC_BTS'] + " Low: " + dadosAtualizadoMinimoCadaDezMinutos['BTC_BTS'] + " High: " + dadosAtualizadoMaximoCadaDezMinutos['BTC_BTS'] + " Close: "+ dadosAtualizadoFechaduraCadaDezMinutos['BTC_BTS'])
+
+    #salvarCandlesNoBanco(10, dadosAtualizadoAberturaCadaDezMinutos, dadosAtualizadoMinimoCadaDezMinutos, dadosAtualizadoMaximoCadaDezMinutos, dadosAtualizadoFechaduraCadaDezMinutos, now)
+    #Atualizando os dados de abertura a Dez minutos para cada ativo
+    for key, value in dadosAtualizadoPorSegundo.items():
+        resertarCandlesPorPeriodicidade(10, key, value)    
+    
+def salvarCandlesNoBanco(periodicidade, dadosAbertura, dadosMinimo, dadosMaximo, dadosFechadura, now):
+    global dadosAtualizadoPorSegundo
+    for key, value in dadosAtualizadoPorSegundo.items():
     #Inserindo Candles no Banco
-        db.execute("Insert into candles (Moeda, Periodicidade ,Open,  Low, High, Close)" + " Values ("+
+        db.execute("Insert into candles (Moeda, Periodicidade, Date_time, Open,  Low, High, Close)" + " Values ("+
                str(key) + ',' +
                str(periodicidade) + ", "+   
                str(now) + ", " +  
-               str(value['Open']) + "," + 
-               str(value['Low']) + ","  + 
-               str(value['High']) + "," + 
-               str(value['Close']) + 
+               str(dadosAbertura[key]) + "," + 
+               str(dadosMinimo[key]) + ","  + 
+               str(dadosMaximo[key]) + "," + 
+               str(dadosFechadura[key]) + 
               ");")
 
 if __name__ == '__main__':
